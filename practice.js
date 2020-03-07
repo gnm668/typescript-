abd06f893fb29afddc5216c0679c5fa7cca9d7fb
Al <nguyenvtalexander@gmail.com>
2020-03-06 20:25:23 -0800

    built in types

--- practice.js
+++ practice.js
@@ -4,3 +4,7 @@
 // console.log(c)
 var d = 'hello';
 console.log(typeof d);
+var e = 'hi';
+var f = 4;
+var g = true;
+console.log(typeof e, typeof f, typeof g);
--- practice.ts
+++ practice.ts
@@ -3,5 +3,10 @@
 // let c = a + b
 // console.log(c)
 
-let d: any = 'hello'
-console.log(typeof d);
+// let d: any = 'hello'
+// console.log(typeof d);
+
+let e: string = 'hi';
+let f: number = 4;
+let g: boolean = true
+console.log(typeof e,typeof f,typeof g)
